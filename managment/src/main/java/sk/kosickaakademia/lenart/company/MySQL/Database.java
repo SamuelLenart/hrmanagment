94d5c89fc413603c86607fffdf8c8cb5d7c02c17
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-03-02 13:02:06 +0100

    Added connection

---
+++ managment/src/main/java/sk/kosickaakademia/lenart/company/MySQL/Database.java
@@ -0,0 +1,42 @@
+package sk.kosickaakademia.lenart.company.MySQL;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Database {
+
+    public Connection getConnection() {
+        try {
+            Properties properties = new Properties();
+            InputStream loader = getClass().getClassLoader().getResourceAsStream("database.properties");
+            properties.load(loader);
+            String url = properties.getProperty("url");
+            String username = properties.getProperty("username");
+            String password = properties.getProperty("password");
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            if (connection != null) {
+                System.out.println("Connected");
+                return connection;
+            } else {
+                System.out.println("Connection failed");
+            }
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }return null;
+    }
+
+    public Connection disconnect(Connection connection){
+        try {
+            if(connection != null){
+                System.out.println("Goodbye");
+                connection.close();
+            }
+        }
+    }
+}
