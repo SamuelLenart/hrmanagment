51c8c0f79b14642ef1ea45a783638147133e8a33
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-03-09 10:21:33 +0100

    add MyFirstServer

---
+++ MyFirstServer/src/main/java/sk/kosickaakademia/lenart/Controller.java
@@ -0,0 +1,36 @@
+package sk.kosickaakademia.lenart;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/hello")
+    public String getHello(){
+        return "Hello there";
+    }
+
+    @RequestMapping("/time")
+    public String currentTime(){
+        return new Date().toString();
+    }
+
+    @RequestMapping("/hello/sk")
+    public String getHelloSk(){
+        return "Ahoj ty tam";
+    }
+
+    @RequestMapping("/hi/test")
+    public String getHiTest(){
+        return "This is a test.";
+    }
+
+    @RequestMapping("/hi/{username}")
+    public String getWithName(@PathVariable String username){
+        return "Hi "+username+". How are u doing?";
+    }
+}
