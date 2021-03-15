d557201cab13bb67b97d32ece7fab84a0973c131
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-03-15 11:19:12 +0100

    :

--- hrmanagment/src/main/java/sk/kosickaakademia/lenart/company/log/LOG.java
+++ hrmanagment/src/main/java/sk/kosickaakademia/lenart/company/log/LOG.java
@@ -1,2 +1,13 @@
-package sk.kosickaakademia.lenart.company.log;public class LOG {
+package sk.kosickaakademia.lenart.company.log;
+
+public class LOG {
+    public void error(String msg){
+        System.out.println("[ERROR] : "+msg);
+    }
+    public void print(String msg){
+        System.out.println("[OK] : "+msg);
+    }
+    public void info(String msg){
+        System.out.println("[INFO] : "+msg);
+    }
 }
