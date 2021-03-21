3cd13fc0a360edbed0a272533b6aa1b0e55b9b6a
Samuel Lenart <samuel.lenart@kosickaakademia.sk>
2021-03-21 19:40:55 +0100

    .

--- MyOwnDatabase/src/main/java/sk/kosickaakademia/lenart/Controller.java
+++ MyOwnDatabase/src/main/java/sk/kosickaakademia/lenart/Controller.java
@@ -4,13 +4,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class Controller {
     Log log = new Log();
-
+    @PostMapping("games/add")
     public ResponseEntity<String> insertNewGame(@RequestBody String data) {
         try {
             JSONObject object = (JSONObject) new JSONParser().parse(data);
@@ -45,7 +46,50 @@ public class Controller {
             return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).
                     body(obj.toJSONString());
         }
-        return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON).
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).
                 body(null);
+    }
+    @GetMapping("games")
+    public ResponseEntity<String> ShowAllGames(@RequestBody String data){
+        List<Game> list = new Database().showAllGames();
+        String json = new Game().getJSON(list);
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(json);
+    }
+    @PutMapping("/games/{id}")
+    public ResponseEntity<String> changeGame(@PathVariable int id,@RequestBody String body) {
+        JSONObject o=new JSONObject();
+        try {
+            o= (JSONObject) new JSONParser().parse(body);
+            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("");
+        }catch(ParseException e) {
+            e.printStackTrace();
+        }
+        String data=String.valueOf(o.get("newGame"));
+        System.out.println("data:"+data);
+        if(data.equalsIgnoreCase("null")){
+            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("");
+        }
+        String newGame = String.valueOf(data);
+        if(newGame==null){
+            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("");
+        }
+        boolean result = new Database().changeGame(id,newGame);
+        if(result){
+            return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("");
+        }else{
+            return ResponseEntity.status(404).contentType(MediaType.APPLICATION_JSON).body("");
+        }
+    }
+    @RequestMapping(value="/game/{id}",method=RequestMethod.DELETE)
+    public ResponseEntity<String> deleteGame(@PathVariable int id){
+        if (database.getGameById(id)==null){
+            JSONObject object=new JSONObject();
+            object.put("ERROR","Game not found");
+            log.error("Game not found");
+            return ResponseEntity.status(404).contentType(MediaType.APPLICATION_JSON).body(object.toJSONString());
+        }
+        database.deleteGame(id);
+        log.print("Game deleted");
+        return ResponseEntity.status(204).contentType(MediaType.APPLICATION_JSON).body(null);
     }
 }
--- MyOwnDatabase/src/main/java/sk/kosickaakademia/lenart/Database.java
+++ MyOwnDatabase/src/main/java/sk/kosickaakademia/lenart/Database.java
@@ -3,7 +3,9 @@ package sk.kosickaakademia.lenart;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
 public class Database {
@@ -11,6 +13,7 @@ public class Database {
     private final String query="INSERT INTO user (name, created, genre, players) VALUES ( ?, ?, ?, ?)";
     private final String INSERTQUERY = "INSERT INTO user (name, created, genre, players)" +
             "VALUES (?, ?, ?, ?)";
+    private final String insertNewGame = "INSERT INTO gamedatabase (DataName, DataGenre, DataPlayers) VALUES(?, ?, ?)";
     public Connection getConnection(){
         try {
             Properties props = new Properties();
@@ -37,5 +40,78 @@ public class Database {
                 log.error(throwables.toString());
             }
         }
+    }
+    public boolean insertNewGame(Game game){
+        Connection conn=getConnection();
+        if(conn!=null){
+            try{
+                PreparedStatement ps=conn.prepareStatement(insertNewGame);
+                ps.setString(1, game.getName());
+                ps.setString(2,game.getGenre());
+                ps.setString(3, game.getPlayers());
+                int result=ps.executeUpdate();
+                closeConnection(conn);
+                log.print("New Game was added to Database.");
+                return result==1;
+            }catch(SQLException ex){
+                log.error(ex.toString());
+            }
+        }
+        return false;
+    }
+    public List<Game> showAllGames(){
+        try (Connection conn = getConnection()) {
+            if (conn != null) {
+                PreparedStatement ps = conn.prepareStatement(showAllGames);
+                return executeSelect(ps);
+            }
+        }catch(Exception ex) {
+        }
+        return null;
+    }
+    public boolean changeGame(int id,String newGame){
+        if (id < 0)
+            return false;
+        try (Connection conn=getConnection()){
+            if (conn != null) {
+                PreparedStatement ps = conn.prepareStatement(changeGame);
+                ps.setString(1, newGame);
+                ps.setInt(2, id);
+                int update=ps.executeUpdate();
+                log.print("Updated game for id: " + id);
+                return update==1;
+            }
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+        return false;
+    }
+    public Game getGameById(int id){
+        try (Connection conn = getConnection()) {
+            if (conn != null) {
+                PreparedStatement ps = conn.prepareStatement(gameByID);
+                ps.setInt(1, id);
+                return (Game) executeSelect(ps);
+            }
+        }catch(Exception ex) {
+        }
+        return null;
+    }
+    public boolean deleteGame(int id){
+        if (getGameById(id) == null){
+            log.error("No game found");
+            return false;
+        }
+        try (Connection conn = getConnection()) {
+            PreparedStatement ps = conn.prepareStatement(deleteGame);
+            ps.setInt(1, id);
+            if (ps.executeUpdate() == 1){
+                log.print("Deleted game: " + id);
+                return true;
+            }
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+        return false;
     }
 }
